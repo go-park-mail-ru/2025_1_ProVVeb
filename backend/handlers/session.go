66cf7ca1168c92f3e1fb7ec1850b770fb3f84d24
Samoilov Anton <samanton43@ya.ru>
2025-03-10 13:19:39 +0300

    jjj

--- backend/handlers/session.go
+++ backend/handlers/session.go
@@ -80,7 +80,7 @@ func (u *SessionHandler) LoginUser(w http.ResponseWriter, r *http.Request) {
 		Name:     "session_id",
 		Value:    SID,
 		HttpOnly: true,
-		Secure:   true,
+		Secure:   false,
 		Expires:  time.Now().Add(10 * time.Hour),
 		Path:     "/",
 		// Domain:   "localhost",
