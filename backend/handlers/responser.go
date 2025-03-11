22c8c6577bc99c9363db7efb2fc45debe21eff16
Samoilov Anton <samanton43@ya.ru>
2025-03-11 20:18:04 +0300

    edited

--- backend/handlers/responser.go
+++ backend/handlers/responser.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-func makeResponse(w http.ResponseWriter, statusCode int, v any) {
+func makeResponse(w http.ResponseWriter, statusCode int, v interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
 	json.NewEncoder(w).Encode(v)
