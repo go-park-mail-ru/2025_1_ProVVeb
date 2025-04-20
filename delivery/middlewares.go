a68149104a1c67f59217029411937461aa85fbfb
Fedin Andrey <fedinaa1@student.bmstu.ru>
2025-04-20 16:33:32 +0300

    Added check

--- delivery/middlewares.go
+++ delivery/middlewares.go
@@ -11,18 +11,20 @@ func AdminAuthMiddleware(u *SessionHandler) mux.MiddlewareFunc {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			session, err := r.Cookie("session_id")
+
 			if err != nil {
-				fmt.Println("no auth at", r.URL.Path)
+				fmt.Println("no cookie for ", r.URL.Path)
 				http.Redirect(w, r, "/", http.StatusFound)
 				return
 			}
 
-			_, err = u.LoginUC.GetSession(session.Value)
+			value, err := u.LoginUC.GetSession(session.Value)
 			if err != nil {
 				fmt.Println("no auth at", r.URL.Path)
 				http.Redirect(w, r, "/", http.StatusFound)
 				return
 			}
+			fmt.Println(value, session)
 			next.ServeHTTP(w, r)
 		})
 	}
