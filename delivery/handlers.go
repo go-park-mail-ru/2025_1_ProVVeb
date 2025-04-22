c35f4c5be69b10636a767d0d8e127e0bba99eac4
Fedin Andrey <fedinaa1@student.bmstu.ru>
2025-04-22 21:42:55 +0300

    Error fix

--- delivery/handlers.go
+++ delivery/handlers.go
@@ -432,14 +432,14 @@ func (uh *UserHandler) DeleteUser(w http.ResponseWriter, r *http.Request) {
 
 func (gh *GetHandler) GetProfile(w http.ResponseWriter, r *http.Request) {
 	userIDRaw := r.Context().Value(userIDKey)
-	profileId, ok := userIDRaw.(int)
+	profileId, ok := userIDRaw.(uint32)
 	if !ok {
 		MakeResponse(w, http.StatusUnauthorized, map[string]string{"message": "You don't have access"})
 		http.Error(w, "Unauthorized", http.StatusUnauthorized)
 		return
 	}
 
-	profile, err := gh.GetProfileUC.GetProfile(profileId)
+	profile, err := gh.GetProfileUC.GetProfile(int(profileId))
 	if err != nil {
 		MakeResponse(w, http.StatusInternalServerError, map[string]string{"message": fmt.Sprintf("Error getting profile: %v", err)})
 		return
