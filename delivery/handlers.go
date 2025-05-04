2fb5cfe3f8898f7ccec1c9a5f767d904c0b1f407
Fedin Andrey <fedinaa1@student.bmstu.ru>
2025-05-04 18:42:25 +0300

    Fix in UpdateProfile

--- delivery/handlers.go
+++ delivery/handlers.go
@@ -508,17 +508,7 @@ func (ph *ProfilesHandler) UpdateProfile(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	if profile.ProfileId != 0 && int(profileId) != profile.ProfileId {
-		ph.Logger.WithFields(&logrus.Fields{
-			"request_profile_id":  profileId,
-			"provided_profile_id": profile.ProfileId,
-		}).Warn("profile ID mismatch")
-
-		MakeResponse(w, http.StatusUnauthorized,
-			map[string]string{"message": "You don't have access for this"},
-		)
-		return
-	}
+	profile.ProfileId = int(profileId)
 
 	table_profile, err := ph.GetProfileUC.GetProfile(int(profileId))
 	if err != nil {
