f075cb9eacc7c0c5510289fc036c2d54e4426472
Fedin Andrey <fedinaa1@student.bmstu.ru>
2025-05-11 19:20:45 +0300

    Small setup change

--- repository/db/configure/set_config.sh
+++ repository/db/configure/set_config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cp /etc/postgresql/16/main/postgresql.conf /etc/postgresql/16/main/postgresql.conf.backup
+sudo cp /etc/postgresql/16/main/postgresql.conf /etc/postgresql/16/main/postgresql.conf.backup
 
-cp config.conf /etc/postgresql/16/main/postgresql.conf
+sudo cp config.conf /etc/postgresql/16/main/postgresql.conf
 
 sudo systemctl restart postgresql 
