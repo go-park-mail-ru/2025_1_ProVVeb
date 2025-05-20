e840afcb2265072d42805ec5d3779964629ac696
Samoilov Anton <samanton43@ya.ru>
2025-05-20 22:35:07 +0300

    added grafana works

--- delivery/utils.go
+++ delivery/utils.go
@@ -3,10 +3,24 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"regexp"
+	"strings"
 )
 
 func MakeResponse(w http.ResponseWriter, statusCode int, v interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
 	json.NewEncoder(w).Encode(v)
+}
+
+func GetUrlForMetrics(url string) string {
+	pattern := `^\d+$`
+	re := regexp.MustCompile(pattern)
+	newUrl := strings.Split(url, "/")
+	for i, s := range newUrl {
+		if re.MatchString(s) {
+			newUrl[i] = "{digits}"
+		}
+	}
+	return strings.Join(newUrl, "/")
 }
